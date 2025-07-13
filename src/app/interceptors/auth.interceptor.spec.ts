f16f7c35aa06280ceec9e4dc343719d1b51015ab
AKZ <75739105+darkelfs56@users.noreply.github.com>
2025-07-14 01:10:12 +0800

    add auth interceptor

---
+++ src/app/interceptors/auth.interceptor.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { AuthInterceptor } from './auth.interceptor';
+
+describe('AuthInterceptor', () => {
+  let interceptor: AuthInterceptor;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    interceptor = TestBed.inject(AuthInterceptor);
+  });
+
+  it('should be created', () => {
+    expect(interceptor).toBeTruthy();
+  });
+});
